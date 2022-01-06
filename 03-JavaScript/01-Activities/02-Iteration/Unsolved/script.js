7013934f9a1e3d482400985a1a9e940bc2c73b52
hazel-jimenez <95867111+hazel-jimenez@users.noreply.github.com>
2022-01-06 18:33:12 -0500

    update script.js

--- 03-JavaScript/01-Activities/02-Iteration/Unsolved/script.js
+++ 03-JavaScript/01-Activities/02-Iteration/Unsolved/script.js
@@ -1,1 +1,12 @@
 // TODO: Write Your JavaScript Code Here
+var students = ['Marc', 'Jared', 'Vulong', 'Michelle', 'Ernest'];
+
+
+console.log(students.length);
+
+
+
+for (var 1 = 0; 1 < students.length; 1++) {
+
+    console.log('Great to see you,' + students[1] + '!');
+}
