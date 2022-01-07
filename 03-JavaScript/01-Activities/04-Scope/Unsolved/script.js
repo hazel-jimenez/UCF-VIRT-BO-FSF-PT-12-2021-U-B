56b73812a4385dc3ec1e1c0ae2bf73bd768cd897
hazel-jimenez <95867111+hazel-jimenez@users.noreply.github.com>
2022-01-07 08:51:05 -0500

    soliving

--- 03-JavaScript/01-Activities/04-Scope/Unsolved/script.js
+++ 03-JavaScript/01-Activities/04-Scope/Unsolved/script.js
@@ -1,4 +1,6 @@
 // TODO: Declare variable 'shout' with the value 'Shout' so it's available to `justShout` and `shoutItAllOut` functions
+var shout = 'shout'
+
 function justShout() {
   console.log(shout + ', ' + shout);
   return;
@@ -20,6 +22,7 @@ function sayLions() {
 }
 
 function sayTigers() {
+  var animal = "Tiger"
   console.log('and ' + animal + ' and ');
   return;
 }
@@ -28,7 +31,6 @@ function sayTigers() {
 var bears = 'Bears';
 
 function sayBears() {
-  var bears = 'Pandas';
   console.log(bears + '! OH MY!');
   return;
 }
